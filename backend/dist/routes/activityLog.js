f8aceff8cd50b214b143662ed16f673a07454588
Muhammad Zeeshan <98173945+zeeshan038@users.noreply.github.com>
2025-12-23 17:27:52 +0500

    Delete backend/dist directory

--- backend/dist/routes/activityLog.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { getAllActivityLogs } from "../controllers/activityLogs.js";
-import { verifyUser } from "../middlewares/verifyUser.js";
-router.use(verifyUser);
-router.get("/all", getAllActivityLogs);
-export default router;
--- backend/dist/utils/Methods.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-export const genrateToken = (id) => {
-    const secret = process.env.JWT_SECRET || "default_secret";
-    console.log(`[Token Gen] Using secret starting with: ${secret.substring(0, 4)}...`);
-    return jwt.sign({ id }, secret);
-};
